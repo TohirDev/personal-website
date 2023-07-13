591cd45e4160901a16966020d575fe4635c77471
Tohir <isakulovtohir902@gmail.com>
2023-07-13 20:35:01 +0500

    begginning of personal website

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function App() {
+  return (
+    <>
+      <Typography variant="h1">hi i hope it is working</Typography>
+    </>
+  );
+}
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <h1>hi</h1>
+      <Box>he</Box>
+    </>
+  );
+}
+
+export default App;
